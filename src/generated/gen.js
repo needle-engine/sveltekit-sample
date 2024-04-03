2c2d2b9da0cab3668c80f637c6c2931dedf9bb77
Krystof <kipash612@gmail.com>
2024-04-03 17:29:51 +0200

    samples-update-16

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -1,16 +1,14 @@
-﻿import "./register_types.ts"
+﻿globalThis["needle:dependencies:ready"] = import("./register_types.ts")
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/SvelteKitSample.glb?v=1693232900157");
+needle_exported_files.push("assets/SvelteKitSample.glb?v=1712157794626");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1693232900157");
+		needleEngine.setAttribute("hash", "1712157794626");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
-
-console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools\ —\ Version\ 3\.12\.0-pre\.2");
