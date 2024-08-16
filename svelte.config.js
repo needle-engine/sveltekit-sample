5d8d1d9434678415fb8d5064aa2d68494bddb543
Marcel Wiessler <marwie@users.noreply.github.com>
2024-08-16 15:47:49 +0200

    Update svelte.config.js: replace "base" with "relative: true"

--- svelte.config.js
+++ svelte.config.js
@@ -26,7 +26,7 @@ const config = {
             strict: true,
         }),
 		paths: {
-			base: '/sveltekit-sample',
+			relative: true
 		},
 		prerender: {
 			// Not needed because the models overview page has links to all the models – and sveltekit will automatically pre-render all reachable links!
