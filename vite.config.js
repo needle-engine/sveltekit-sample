9ceb5a9a55f7440ae2843f460cc57bdf46689ee0
Marcel Wiessler <marcel@gaisterhand.de>
2025-04-02 17:26:36 +0200

    workflow

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
-
-
 import { sveltekit } from '@sveltejs/kit/vite';
 
 // https://github.com/sapphi-red/vite-plugin-static-copy#usage
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-const files = [];
+const files = new Array();
 
 export default defineConfig(async ({ command }) => {
     const { needlePlugins, useGzip, loadConfig } = await import("@needle-tools/engine/plugins/vite/index.js");
